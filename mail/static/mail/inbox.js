c683850df6d0ca78f7ced0abd0319db7665c87c5
Lumino7 <112755263+Lumino7@users.noreply.github.com>
2023-10-05 15:00:45 +0000

    Thu, Oct 5, 2023, 4:00 PM +01:00

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -39,9 +39,7 @@ function load_mailbox(mailbox) {
   // Show the mailbox name
   emailsView.innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 
-  // You’ll likely want to make a GET request to /emails/<mailbox> to request the emails for a particular mailbox.
   // When a mailbox is visited, the application should first query the API for the latest emails in that mailbox.
-  // When a mailbox is visited, the name of the mailbox should appear at the top of the page (this part is done for you).
   // Each email should then be rendered in its own box (e.g. as a <div> with a border) that displays who the email is from, what the subject line is, and the timestamp of the email.
   // If the email is unread, it should appear with a white background. If the email has been read, it should appear with a gray background.
 }
