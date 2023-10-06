d0db944a4452c159b662593aa45a3850aec778da
Lumino7 <112755263+Lumino7@users.noreply.github.com>
2023-10-06 10:37:45 +0000

    Fri, Oct 6, 2023, 11:37 AM +01:00

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -191,6 +191,7 @@ function show_email_content(id) {
   let subject = document.createElement('p');
   header.appendChild(subject);
   subject.innerText = `Subject: ${result.subject}`;
+  subject.classList.add('pr-2','font-weight-bold');
 
   let timestamp = document.createElement('p');
   header.appendChild(timestamp);
